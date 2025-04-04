3320fa3bdea16d5903611a7ee0ec1785d97cb0cd
DhruvPatel284 <dhruv156328@gmail.com>
2025-04-04 15:50:37 +0530

    change in requirements

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,1 +1,3 @@
-export const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:5000"; 
+//@ts-ignore
+// export const BACKEND_URL = "http://localhost:5000"; 
+export const BACKEND_URL = "https://social-media-analysis-dashboard-1.onrender.com"; 
